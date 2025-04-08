bebc810f35f82f02076383911557eb44f5194c1a
Vaibhav Pratap Singh <v8v88v8v88@protonmail.com>
2025-04-08 21:24:29 +0530

    new fish config

--- fish/config.fish
+++ fish/config.fish
@@ -18,10 +18,11 @@ function fu; flatpak update; end
 
 # Set the environment variable for Wayland
 set -x ELECTRON_OZONE_PLATFORM_HINT auto
-set -Ux QT_QPA_PLATFORM wayland
 set -Ux SDL_VIDEODRIVER wayland
+set -Ux QT_QPA_PLATFORM wayland
 set -Ux QT_WAYLAND_DISABLE_WINDOWDECORATION 0
-
+set -Ux QT_QPA_PLATFORMTHEME qt5ct
+set -e QT_STYLE_OVERRIDE
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"set --export PATH $BUN_INSTALL/bin $PATH
@@ -29,3 +30,12 @@ set --export BUN_INSTALL "$HOME/.bun"set --export PATH $BUN_INSTALL/bin $PATH
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+function vlc
+    env QT_QPA_PLATFORM=wayland vlc $argv
+end
+
+function cursor
+    nohup /home/v8v88v8v88/.local/bin/Cursor $argv >/dev/null 2>&1 & disown
+end
+set -gx PATH $HOME/.local/bin $PATH
